f27aafeb78631370e24ee1d168ee6bc31897af45
Emmanuel <emmanuelramello@gmail.com>
2023-05-12 23:22:41 -0300

    Subiendo el proyecto de alura

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-desarrollando-con-JavaScript-4-
-Curso de React: desarrollando con javascript de Alura Latam G4
--- first-react-app/src/componentes/Boton/index.js
+++ first-react-app/src/componentes/Boton/index.js
@@ -1,8 +1,7 @@
-import './Boton.css';
+import "./Boton.css"
 
 const Boton = (props) => {
-//  return <button className='boton' >{ props.texto }</button> .Esta es una forma
-    return <button className='boton' >{ props.children }</button>
+    return <button className="boton">{props.children}</button>
 }
 
-export default Boton;
+export default Boton
